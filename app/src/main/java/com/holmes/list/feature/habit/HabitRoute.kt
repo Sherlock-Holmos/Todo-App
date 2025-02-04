dab1197f582a9f793b153e25cec303834cf856e9
Sherlock-Holmes <wuruijie1412@126.com>
2025-02-05 00:17:12 +0800

    HabitPage Initialized

--- app/src/main/java/com/holmes/list/feature/habit/HabitRoute.kt
+++ app/src/main/java/com/holmes/list/feature/habit/HabitRoute.kt
@@ -1,8 +1,16 @@
 package com.holmes.list.feature.habit
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.holmes.list.feature.habit.component.HabitTitleBar
+import com.holmes.list.feature.week.component.WeekListContainer
+import com.holmes.list.feature.week.component.WeekTitleBar
 import com.holmes.list.ui.theme.ListTheme
 
 /**
@@ -18,7 +26,18 @@ fun HabitRoute() {
  */
 @Composable
 fun HabitScreen() {
-    Text(text = "习惯页")
+    Scaffold(
+        topBar = {
+            HabitTitleBar()
+        }
+    ) {paddingValues ->
+        Box(modifier = Modifier
+            .padding(paddingValues)
+            .fillMaxSize()
+        ) {
+
+        }
+    }
 }
 
 /**
---
+++ app/src/main/java/com/holmes/list/feature/habit/component/HabitTitleBar.kt
@@ -0,0 +1,32 @@
+package com.holmes.list.feature.habit.component
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.outlined.DateRange
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.sp
+
+/**
+ * 习惯页标题栏
+ */
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HabitTitleBar() {
+    TopAppBar(title = { Text(text = "Habit", fontSize = 20.sp) }, actions = {
+        IconButton(onClick = { /*TODO*/ }) {
+            Icon(imageVector = Icons.Outlined.DateRange, contentDescription = null)
+        }
+        IconButton(onClick = { /*TODO*/ }) {
+            Icon(imageVector = Icons.Filled.List, contentDescription = null)
+        }
+        IconButton(onClick = { /*TODO*/ }) {
+            Icon(imageVector = Icons.Filled.MoreVert, contentDescription = null)
+        }
+    })
+}
