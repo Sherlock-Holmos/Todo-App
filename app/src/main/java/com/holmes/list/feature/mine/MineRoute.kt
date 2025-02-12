b615f2417dcfb374a608fc8ce936ec9dfc9c69c2
Sherlock-Holmes <wuruijie1412@126.com>
2025-02-12 23:51:19 +0800

    MinePage delete Topbar

--- app/src/main/java/com/holmes/list/feature/mine/MineRoute.kt
+++ app/src/main/java/com/holmes/list/feature/mine/MineRoute.kt
@@ -7,29 +7,15 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material3.Card
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.holmes.list.feature.mine.component.MineTitleBar
-import com.holmes.list.ui.theme.ListTheme
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.automirrored.filled.Send
 import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material.icons.filled.Create
 import androidx.compose.material.icons.filled.DateRange
@@ -37,12 +23,22 @@ import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Notifications
-import androidx.compose.material.icons.filled.Send
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.ShoppingCart
-import androidx.compose.material3.Divider
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
-
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.holmes.list.ui.theme.ListTheme
 
 
 /**
@@ -58,7 +54,7 @@ fun MineRoute() {
  */
 @Composable
 fun MineScreen() {
-    Scaffold() { paddingValues ->
+    Scaffold{ paddingValues ->
         Box(
             modifier = Modifier
                 .padding(paddingValues)
@@ -148,13 +144,13 @@ fun MenuList() {
         ) {
             Column(modifier = Modifier.padding(16.dp)) {
                 MenuItem(icon = Icons.Filled.CheckCircle, title = "我的勋章") { }
-                Divider()
+                HorizontalDivider()
                 MenuItem(icon = Icons.Filled.Menu, title = "待办周报") { }
-                Divider()
-                MenuItem(icon = Icons.Filled.Send, title = "导入与关联应用") { }
-                Divider()
+                HorizontalDivider()
+                MenuItem(icon = Icons.AutoMirrored.Filled.Send, title = "导入与关联应用") { }
+                HorizontalDivider()
                 MenuItem(icon = Icons.Filled.Info, title = "使用手册") { }
-                Divider()
+                HorizontalDivider()
                 MenuItem(icon = Icons.Filled.FavoriteBorder, title = "关注我") { }
             }
         }
@@ -185,7 +181,6 @@ fun RowItem(icon: ImageVector, title: String, onClick: () -> Unit) {
 }
 
 
-
 /**
  * 单个菜单项
  */
@@ -195,8 +190,7 @@ fun MenuItem(icon: ImageVector, title: String, onClick: () -> Unit) {
         modifier = Modifier
             .fillMaxWidth()
             .clickable(onClick = onClick) // 点击事件
-            .padding(vertical = 12.dp),
-        verticalAlignment = Alignment.CenterVertically
+            .padding(vertical = 12.dp), verticalAlignment = Alignment.CenterVertically
     ) {
         Icon(imageVector = icon, contentDescription = title, modifier = Modifier.size(24.dp))
         Spacer(modifier = Modifier.width(16.dp)) // 图标和文字之间的间距
