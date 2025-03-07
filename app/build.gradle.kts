3a9e420d1bc0443c608bebd546b10f793bfeac32
Sherlock-Holmos <wuruijie1412@126.com>
2025-03-07 15:33:59 +0800

    Import Retrofit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,12 +53,18 @@ android {
 dependencies {
     implementation(libs.generativeai)
     implementation(libs.protolite.well.known.types)
+    //implementation(libs.androidx.media3.session)
+    //ROOM
     val room_version = "2.6.1"
     implementation("androidx.room:room-runtime:$room_version")
     ksp("androidx.room:room-compiler:$room_version")
     implementation("androidx.room:room-ktx:$room_version")
     val nav_version = "2.8.4"
     implementation("androidx.navigation:navigation-compose:$nav_version")
+    // Retrofit
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
